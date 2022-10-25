99ee3835f26811eba4f95fad1328667d076fa15c
Filip Stojiljkovic <filip.stojiljkovic90@yahoo.com>
2022-10-25 22:32:27 +0200

    protonvpn script done

--- proton-script.sh
+++ proton-script.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 vpn_servers=(IS-IL#1 IL#5 IL#6 IL#7 IL#8 CH-IL#1 IL#9 IL#10 IL#11 IL#12 CY#1 CY#2 CY#3 CY#4 CH-CY#1)
-index=0
+index=$(<index.txt)
 
+protonvpn-cli connect ${vpn_servers[$index]}
+index=$((index+1))
 if [[ index -eq ${#vpn_servers[@]} ]];
 then
   index=0
 fi
-protonvpn-cli connect ${vpn_servers[$index]}
-index=$((index+1))
+echo $index > index.txt
