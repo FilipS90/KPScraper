355e781f4abbe258c1205f74cc8406bbada60002
Filip Stojiljkovic <filip.stojiljkovic90@yahoo.com>
2022-11-14 01:25:18 +0100

    working for all pages

--- README.md
+++ README.md
@@ -2,20 +2,9 @@
 
 TODO:
 
-- error on second page of ads
-File "/home/filips/MyCode/KPScraper/main.py", line 7, in <module>
-    itr.IteratorService().interateOverCategories()
-  File "/home/filips/MyCode/KPScraper/iteratorService.py", line 27, in interateOverCategories
-    self.iterateAdCategory(downloader)
-  File "/home/filips/MyCode/KPScraper/iteratorService.py", line 31, in iterateAdCategory
-    adUrls = self.getAdUrlsFromCurrentPage(self.driver.current_url)
-  File "/home/filips/MyCode/KPScraper/iteratorService.py", line 42, in getAdUrlsFromCurrentPage
-    adUrlsArray = soup.find('div', class_='Grid_col-lg-10__tIdze Grid_col-xs__6oZvU Grid_col-sm__hxOHE Grid_col-md__1bRJZ').find_all('a', class_='Link_link__J4Qd8 Link_inherit___qXEP AdGoldHeader_goldHeader__t_ira')
-AttributeError: 'NoneType' object has no attribute 'find_all'
-
 - make KPScraper completely autonomous
   1) copy proton-script to /bin in Dockerfile
-  2) install protonvpn-cli during initialization (if not installed ...)
+  2) install protonvpn-cli in Dockerfile
 - save point of current progress
 - hashmap for numbers;
 - logger service
--- iteratorService.py
+++ iteratorService.py
@@ -24,41 +24,49 @@ class IteratorService:
 
             btn = self.driver.find_element(By.XPATH, '//*[@id="__next"]/div/div[1]/div/div[3]/div/div/div/div[2]/form/section/div/div[1]/div/section/section/div/span[2]/button')
             self.driver.execute_script("arguments[0].click();",btn)
+            sleep(1) # sleep so above command loads entirely
             self.iterateAdCategory(downloader)
 
     def iterateAdCategory(self, downloader):
         while(True):
-            adUrls = self.getAdUrlsFromCurrentPage(self.driver.current_url)
+            adPageUrl = self.driver.current_url
+            adUrls = self.getAdUrlsFromCurrentPage(adPageUrl)
+            print('printing ad urls')
+            print(adUrls)
             for adUrl in adUrls:
-                print('downloading ad -> ' + adUrl)
                 downloader.downloadImage(gl.HOME_URL + adUrl)
-                sleep(random.randint(3,10))
+                sleep(random.randint(2,5))
                 self.changeVpnOrIncrement(self.saveCounter)
-            self.changePageNumber()
+            self.changePageNumber(adPageUrl)
 
 
     def getAdUrlsFromCurrentPage(self, currentUrl):
         soup = BeautifulSoup(requests.get(currentUrl).text, 'lxml')
-        adUrlsArray = soup.find('div', class_='Grid_col-lg-10__tIdze Grid_col-xs__6oZvU Grid_col-sm__hxOHE Grid_col-md__1bRJZ').find_all('a', class_='Link_link__J4Qd8 Link_inherit___qXEP AdGoldHeader_goldHeader__t_ira')
+        goldAdsUrlsArr = soup.find('div', class_='Grid_col-lg-10__tIdze Grid_col-xs__6oZvU Grid_col-sm__hxOHE Grid_col-md__1bRJZ').find_all('a', class_='Link_link__J4Qd8 Link_inherit___qXEP AdGoldHeader_goldHeader__t_ira')
+        regularAdsUrlsArr = soup.find('div', class_='Grid_col-lg-10__tIdze Grid_col-xs__6oZvU Grid_col-sm__hxOHE Grid_col-md__1bRJZ').find_all('a', class_='Link_link__J4Qd8 Link_inherit___qXEP')
+        adUrlsArr = goldAdsUrlsArr + regularAdsUrlsArr
         resultArr = []
-        for ad in adUrlsArray:
+        for ad in adUrlsArr:
             if '?' in ad['href']:
                 resultArr.append(ad['href'].split('?')[0])
             else:
                 resultArr.append(ad['href'])
         return resultArr
 
-    def changePageNumber(self):
-        currentPage = self.driver.current_url
+    def changePageNumber(self, adPageUrl):
         newPageNum = None
-        if 'page' in currentPage[-6:]:
-            newPageNum = int(currentPage[-1]) + 1
+        if 'page' in adPageUrl[-6:]:
+            newPageNum = int(adPageUrl[-1]) + 1
         else:
             newPageNum = 2
-            self.driver.get(currentPage + '&page=' + str(newPageNum))
+            print('first change')
+            print(adPageUrl + '&page=' + str(newPageNum))
+            self.driver.get(adPageUrl + '&page=' + str(newPageNum))
             return
 
-        forUrlsOtherThanFirstPage = currentPage.split('page=')
+        forUrlsOtherThanFirstPage = adPageUrl.split('page=')
+        print('next page')
+        print(forUrlsOtherThanFirstPage[0] + 'page=' + str(newPageNum))
         self.driver.get(forUrlsOtherThanFirstPage[0] + 'page=' + str(newPageNum))
         
 
