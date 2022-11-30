7459895dcf10e41612581b4a1df3baf8b4eaf62d
Filip Stojiljkovic <filip.stojiljkovic90@yahoo.com>
2022-11-30 23:42:46 +0100

    small refactor

--- globals.py
+++ globals.py
@@ -2,6 +2,7 @@ CATEGORY = ''
 CATEGORIES_LIST_CLASS = 'CategoryList_list__a7SOH'
 CATEGORIES = []
 CATEGORY_IDS = []
+CURRENT_CATEGORY_IDX=None
 DRIVER = None
 FILE_PATH = ''
 HOME_URL = 'https://www.kupujemprodajem.com'
