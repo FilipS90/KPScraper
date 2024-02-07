794063c4cd61a5e148a04d4141ca8b9d6416f39d
FilipS90 <filip.stojiljkovic90@yahoo.com>
2024-02-07 20:51:26 +0100

    removing credentials

---
+++ siteLoginService.py
@@ -0,0 +1,14 @@
+import globals as gl
+from selenium.webdriver.common.by import By
+
+class SiteLoginService:
+
+    def login(self):
+        driver = gl.DRIVER
+        driver.get(gl.HOME_URL)
+        loginBtn = driver.find_element(By.XPATH, '//*[@id="__next"]/div/div[3]/div/div/div[1]/div[1]/section/div/div/ul/li[2]/button')
+        driver.execute_script("arguments[0].click();", loginBtn)
+        driver.find_element(By.XPATH, '//*[@id="email"]').send_keys('-')
+        driver.find_element(By.XPATH, '//*[@id="password"]').send_keys('-')
+        loginBtn = driver.find_element(By.XPATH, '//*[@id="kp-portal"]/div/div/aside/div/div/div[2]/main/div[3]/form/button').click()
+        print('Sign in done')
